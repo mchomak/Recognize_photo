ff7bf3b2b69fd4b889d447fba11637973f888be4
McHomak <kaneevramil7@gmail.com>
2024-06-13 20:14:10 +0300

    gg4

--- config.py
+++
@@ -1,5 +0,0 @@
-# Config={
-#     "api_key":'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',
-#     "proxy1":'http://user177996:utsjlg@166.1.108.101:5096',
-#     "proxy2":'http://user177996:utsjlg@166.1.108.101:5096',
-#     }
